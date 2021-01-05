9c045726de734849edcabb635619ec4131e19040
Chris Linder <thechrislinder@protonmail.com>
2021-01-05 14:31:34 -0500

    bug fixes

--- JOSESwift/Sources/AlgorithmExtensions.swift
+++ JOSESwift/Sources/AlgorithmExtensions.swift
@@ -43,6 +43,8 @@ extension ContentEncryptionAlgorithm {
                 return 32
             case .AES128GCM:
                  return 16
+           default:
+                return 16
            }
        }
 
@@ -86,6 +88,8 @@ extension ContentEncryptionAlgorithm {
                return (inputKey.subdata(in: 0..<16), inputKey.subdata(in: 16..<32))
            case .AES256GCM:
             return (hmacKey: Data(), encryptionKey: inputKey)
+           case .AES128GCM:
+            return (hmacKey: Data(), encryptionKey: inputKey)
            }
        }
 
@@ -96,6 +100,8 @@ extension ContentEncryptionAlgorithm {
            case .A128CBCHS256:
                return hmac.subdata(in: 0..<16)
            case.AES256GCM:
+            return Data()
+           case .AES128GCM:
             return Data()
            }
         
